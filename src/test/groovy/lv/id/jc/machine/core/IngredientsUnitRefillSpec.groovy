a86526a06703481e78a4b8ecca31a579b48bfaa3
Jegors Čemisovs <jegors_cemisovs@epam.com>
2022-10-29 19:06:22 +0300

    created a file for the specification of the unit of ingredients for the coffee machine

---
+++ src/test/groovy/lv/id/jc/machine/core/IngredientsUnitRefillSpec.groovy
@@ -0,0 +1,16 @@
+package lv.id.jc.machine.core
+
+import spock.lang.Narrative
+import spock.lang.Specification
+import spock.lang.Title
+
+@Title('Technician refills ingredients in coffee machine')
+@Narrative('''
+As a technician
+I want to replenish the necessary ingredients in the coffee machine
+So that the coffee machine can prepare the necessary coffee drinks
+''')
+class IngredientsUnitRefillSpec extends Specification {
+
+
+}
