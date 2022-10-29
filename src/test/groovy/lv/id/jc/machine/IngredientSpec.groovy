ffa6867daf8b3fcf53a8ca251a778de6144c3403
Jegors Čemisovs <jegors_cemisovs@epam.com>
2022-10-29 15:04:55 +0300

    implemented specification for ingredient names

--- src/test/groovy/lv/id/jc/machine/IngredientSpec.groovy
+++ src/test/groovy/lv/id/jc/machine/IngredientSpec.groovy
@@ -2,13 +2,23 @@ package lv.id.jc.machine
 
 
 import spock.lang.Specification
+import spock.lang.Subject
+import spock.lang.Title
 
+@Subject(Ingredient)
+@Title('Ingredients needed to make coffee')
 class IngredientSpec extends Specification {
 
-    def 'should have 3 main ingredients'() {
+    def 'should have all main ingredients'() {
 
-        expect: 'all necessary ingredients are present'
+        given: 'description of all ingredients as a set'
+        def ingredients = Ingredient.values()*.description as Set
 
+        expect: 'required ingredient is presented'
+        ingredient in ingredients
+
+        where: 'three main ingredients are'
+        ingredient << ["water", "milk", "coffee beans"]
     }
 
     def 'should set units of measurement for each of the ingredients'() {
