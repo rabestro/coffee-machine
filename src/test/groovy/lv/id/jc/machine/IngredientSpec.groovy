e346e4c132cd454a66db37c69a66c550fdf449f2
Jegors Čemisovs <jegors_cemisovs@epam.com>
2022-10-29 14:49:00 +0300

    added template for specification of ingredients

---
+++ src/test/groovy/lv/id/jc/machine/IngredientSpec.groovy
@@ -0,0 +1,19 @@
+package lv.id.jc.machine
+
+
+import spock.lang.Specification
+
+class IngredientSpec extends Specification {
+
+    def 'should have 3 main ingredients'() {
+
+        expect: 'all necessary ingredients are present'
+
+    }
+
+    def 'should set units of measurement for each of the ingredients'() {
+
+        expect: 'the correct unit of measurement is set for each of the ingredients'
+
+    }
+}
