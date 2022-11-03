d8a34ad5410d4d0dd57796a575a438b06eb54932
Jegors Čemisovs <jegors_cemisovs@epam.com>
2022-11-03 10:51:14 +0200

    update specification for coffee machine replenish

--- src/test/groovy/lv/id/jc/machine/unit/CoffeeMachineReplenishSpec.groovy
+++ src/test/groovy/lv/id/jc/machine/unit/CoffeeMachineReplenishSpec.groovy
@@ -14,27 +14,27 @@ As a technician
 I want to replenish the necessary resources in the coffee machine
 So that the coffee machine can prepare the necessary coffee drinks
 ''')
-@Subject(Engine)
+@Subject(StorageBlock)
 class CoffeeMachineReplenishSpec extends Specification {
 
     def 'should replenish the specified resource in the coffee machine'() {
 
         given: 'resource unit with a particular initial volume of resources'
-        def machine = new Engine()
+        def storage = new StorageBlock()
 
-        with(machine) {
+        with(storage) {
             fill(Water, water)
             fill(Milk, milk)
             fill(CoffeeBeans, beans)
             fill(DisposableCups, cups)
-            fill(Money, money)
+            fill(Cash, money)
         }
 
         when: 'we replenish a particular resource'
-        machine.fill(target, replenishment)
+        storage.fill(target, replenishment)
 
         then: 'the volume of this resource has increased due to the added replenishment'
-        machine.volume(target) == old(machine.volume(target)) + replenishment
+        storage.volume(target) == old(storage.volume(target)) + replenishment
 
         where: 'the initial volume of resources'
         water | milk | beans | cups | money | target | replenishment
