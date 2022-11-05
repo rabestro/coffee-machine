d2d0f7fba4e0823193708500fb9b1bb17f8c16db
Jegors Čemisovs <jegors_cemisovs@epam.com>
2022-11-05 13:08:31 +0200

    add StorageUnitTakeResourcesSpec with title and narrative only

---
+++ src/test/groovy/lv/id/jc/machine/unit/StorageUnitTakeResourcesSpec.groovy
@@ -0,0 +1,15 @@
+package lv.id.jc.machine.unit
+
+import spock.lang.Narrative
+import spock.lang.Specification
+import spock.lang.Title
+
+@Title('The storage device allocates resources to prepare a coffee drink')
+@Narrative('''
+As a control unit
+I want to receive the necessary resources from the storage unit
+So that I will be able to prepare the desired coffee drink
+''')
+class StorageUnitTakeResourcesSpec extends Specification {
+
+}
