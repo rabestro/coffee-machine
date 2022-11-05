ff53f06759c77e8af4c12a7950bc4af8663e3599
Jegors Čemisovs <jegors_cemisovs@epam.com>
2022-11-05 17:29:25 +0200

    add an empty control unit specification for coffee order

---
+++ src/test/groovy/lv/id/jc/machine/unit/ControlUnitBuySpec.groovy
@@ -0,0 +1,22 @@
+package lv.id.jc.machine.unit
+
+import spock.lang.Narrative
+import spock.lang.Specification
+import spock.lang.Title
+
+@Title('The customer buys a coffee drink')
+@Narrative('''
+As a coffee lover
+I want to choose and buy my favorite coffee drink
+So that I can enjoy my beverage
+''')
+class ControlUnitBuySpec extends Specification {
+
+    def 'should prepare coffee drink if enough resources'() {
+
+    }
+
+    def 'should report a lack of resources'() {
+
+    }
+}
