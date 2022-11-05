641af18a4c19aa71206fdd842ba1d09081917961
Jegors Čemisovs <jegors_cemisovs@epam.com>
2022-11-05 10:59:49 +0200

    added sample data for missing resources specification

--- src/test/groovy/lv/id/jc/machine/unit/StorageUnitMissingResourcesSpec.groovy
+++ src/test/groovy/lv/id/jc/machine/unit/StorageUnitMissingResourcesSpec.groovy
@@ -1,9 +1,11 @@
 package lv.id.jc.machine.unit
 
-
+import lv.id.jc.machine.model.Coffee
 import spock.lang.Narrative
 import spock.lang.Specification
 import spock.lang.Title
+
+import static lv.id.jc.machine.model.Resource.*
 
 @Title('The control device queries for the missing coffee drink ingredients')
 @Narrative('''
@@ -24,6 +26,20 @@ class StorageUnitMissingResourcesSpec extends Specification {
         then: 'we get a set containing the missing ingredients'
 
         where: 'resources in storage device, coffee drink and missing ingredients'
-
+        water | milk | beans | cups | drink             | required
+        0     | 0    | 0     | 0    | Coffee.Espresso   | [Water, CoffeeBeans, DisposableCups]
+        0     | 0    | 0     | 0    | Coffee.Latte      | [Water, Milk, CoffeeBeans, DisposableCups]
+        0     | 0    | 0     | 1    | Coffee.Latte      | [Water, Milk, CoffeeBeans]
+        0     | 0    | 20    | 1    | Coffee.Latte      | [Water, Milk]
+        0     | 75   | 20    | 1    | Coffee.Latte      | [Water]
+        350   | 75   | 20    | 1    | Coffee.Latte      | []
+        349   | 74   | 19    | 0    | Coffee.Latte      | [Water, Milk, CoffeeBeans, DisposableCups]
+        360   | 76   | 21    | 2    | Coffee.Latte      | []
+        200   | 100  | 12    | 1    | Coffee.Cappuccino | []
+        199   | 99   | 11    | 0    | Coffee.Cappuccino | [Water, Milk, CoffeeBeans, DisposableCups]
+        455   | 395  | 84    | 0    | Coffee.Cappuccino | [DisposableCups]
+        800   | 650  | 0     | 45   | Coffee.Cappuccino | [CoffeeBeans]
+        750   | 0    | 95    | 7    | Coffee.Cappuccino | [Milk]
+        0     | 300  | 45    | 9    | Coffee.Cappuccino | [Water]
     }
 }
