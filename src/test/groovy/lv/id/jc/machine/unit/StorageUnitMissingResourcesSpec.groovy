71569813db24885f4574aa9e6a617a0d3ab8b336
Jegors Čemisovs <jegors_cemisovs@epam.com>
2022-11-05 09:48:54 +0200

    create an empty specification for missing resources request with title and narrative

---
+++ src/test/groovy/lv/id/jc/machine/unit/StorageUnitMissingResourcesSpec.groovy
@@ -0,0 +1,15 @@
+package lv.id.jc.machine.unit
+
+import spock.lang.Narrative
+import spock.lang.Specification
+import spock.lang.Title
+
+@Title('The control device queries for the missing coffee drink ingredients')
+@Narrative('''
+As control unit
+I want to know if there are enough ingredients to make a coffee drink
+So that I will know if it is possible to prepare a drink
+''')
+class StorageUnitMissingResourcesSpec extends Specification {
+
+}
