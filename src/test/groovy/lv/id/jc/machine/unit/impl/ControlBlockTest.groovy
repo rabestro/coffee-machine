e0790b7f600b64c20eb71a7f98b203c0f57d7eef
Jegors Čemisovs <jegors_cemisovs@epam.com>
2022-11-12 01:07:45 +0200

    add ControlBlockTest

--- src/test/groovy/lv/id/jc/machine/unit/impl/ControlBlockTest.groovy
+++ src/test/groovy/lv/id/jc/machine/unit/impl/ControlBlockTest.groovy
@@ -13,50 +13,65 @@ import spock.lang.Subject
 @UnitTest
 @Subject(ControlBlock)
 class ControlBlockTest extends Specification {
-
-    DisplayUnit displayUnit = new FakeDisplay()
-    StorageUnit storageUnit = Stub()
+    DisplayUnit displayMock = Mock()
+    StorageUnit storageMock = Mock()
     ControlUnit controlUnit
 
-    void setup() {
-        controlUnit = new ControlBlock(displayUnit, storageUnit)
+    def 'should powerOn control block'() {
+
+        given: 'newly created control block'
+        controlUnit = new ControlBlock(displayMock, storageMock)
+
+        expect: 'immediately after creation, the control unit does not work'
+        !controlUnit.isOperate()
+
+        when: 'we turn on the control device'
         controlUnit.powerOn()
-        displayUnit.clear()
-    }
 
-    void cleanup() {
-    }
+        then: 'control device goes into operation'
+        controlUnit.isOperate()
 
+        and: 'the main menu is displayed on the screen'
+        1 * displayMock.accept(ControlState.MainMenu.prompt)
+    }
 
     def 'should process request "exit"'() {
+
+        given: 'newly created control block'
+        controlUnit = new ControlBlock(displayMock, storageMock)
+
+        and: 'we turn on the control device'
+        controlUnit.powerOn()
+
         when:
         controlUnit.process Command.EXIT.name()
 
         then:
-        displayUnit.text.isBlank()
         !controlUnit.isOperate()
     }
 
-    def loadResources(water, milk, beans, cups, cash) {
-        with(storageUnit) {
+    def 'should process request "remaining"'() {
+
+        given:
+        StorageUnit storageStub = Stub() {
             volume(Resource.Water) >> water
             volume(Resource.Milk) >> milk
             volume(Resource.CoffeeBeans) >> beans
             volume(Resource.DisposableCups) >> cups
             volume(Resource.Cash) >> cash
         }
-    }
 
-    def 'should process request "remaining"'() {
+        def fakeDisplay = new FakeDisplay()
 
-        given:
-        loadResources water, milk, beans, cups, cash
+        and:
+        controlUnit = new ControlBlock(fakeDisplay, storageStub)
+        controlUnit.powerOn()
 
         when:
         controlUnit.process Command.REMAINING.name()
 
         then:
-        displayUnit.contains report
+        fakeDisplay.contains report
 
         where: 'the volume of resources in the storage unit'
         water | milk | beans | cups | cash
@@ -75,87 +90,154 @@ class ControlBlockTest extends Specification {
 
     def 'should process request "take"'() {
 
-        given:
-        loadResources water, milk, beans, cups, cash
+        given: 'control block in main menu state'
+        controlUnit = new ControlBlock(displayMock, storageMock)
+        controlUnit.powerOn()
 
         when:
         controlUnit.process Command.TAKE.name()
 
         then:
-        displayUnit.contains report
+        storageMock.volume(Resource.Cash) >> cash
+
+        and:
+        1 * storageMock.withdrawCash()
+
+        and:
+        1 * displayMock.accept(report)
 
         where: 'the volume of resources in the storage unit'
-        water | milk | beans | cups | cash | report
-        0     | 0    | 0     | 0    | 0    | "I gave you \$0"
-        400   | 540  | 20    | 9    | 550  | "I gave you \$$cash"
+        cash << [0, 550, 980]
+        report = "I gave you \$$cash"
     }
 
     def 'should process request "fill"'() {
-        given:
-        loadResources water, milk, beans, cups, cash
+
+        given: 'control block in main menu state'
+        controlUnit = new ControlBlock(displayMock, storageMock)
+        controlUnit.powerOn()
 
         when:
         controlUnit.process Command.FILL.name()
 
         then:
-        displayUnit.contains ControlState.FillWater.prompt
+        1 * displayMock.accept(ControlState.FillWater.prompt)
+    }
 
-        where: 'the volume of resources in the storage unit'
-        water | milk | beans | cups | cash
-        0     | 0    | 0     | 0    | 0
-        400   | 540  | 20    | 9    | 550
+    def 'should process request in "fill water" control mode'() {
+
+        given: 'control block in fill water state'
+        controlUnit = new ControlBlock(displayMock, storageMock)
+        controlUnit.powerOn()
+        controlUnit.process Command.FILL.name()
+
+        when:
+        controlUnit.process request
+
+        then:
+        1 * storageMock.fill(Resource.Water, waterVolume)
+
+        and:
+        1 * displayMock.accept(ControlState.FillMilk.prompt)
+
+        where:
+        request << ['0', '2000', '5000']
+
+        and:
+        waterVolume = request.toInteger()
+    }
+
+    def 'should process request in "fill milk" control state'() {
+
+        given:
+        controlUnit = new ControlBlock(displayMock, storageMock)
+        controlUnit.powerOn()
+        controlUnit.process Command.FILL.name()
+        controlUnit.process waterVolume
+
+        when:
+        controlUnit.process request
+
+        then:
+        1 * storageMock.fill(Resource.Milk, volume)
+
+        and:
+        1 * displayMock.accept(ControlState.FillBeans.prompt)
+
+        where:
+        waterVolume = '9000'
+        request << ['0', '2000', '5000']
+
+        and:
+        volume = request.toInteger()
     }
 
 
+    def 'should process request in "fill coffee beans" control state'() {
+        given:
+        controlUnit = new ControlBlock(displayMock, storageMock)
+        controlUnit.powerOn()
+
+        and:
+        controlUnit.process Command.FILL.name()
+        controlUnit.process someVolume
+        controlUnit.process someVolume
+
+        when:
+        controlUnit.process request
+
+        then:
+        1 * storageMock.fill(Resource.CoffeeBeans, volume)
+
+        and:
+        1 * displayMock.accept(ControlState.FillCups.prompt)
+
+        where:
+        someVolume = '9000'
+        request << ['0', '2000', '5000']
+
+        and:
+        volume = request.toInteger()
+    }
+
+
+    def 'should process request in "fill disposable cups" control state'() {
+        given:
+        controlUnit = new ControlBlock(displayMock, storageMock)
+
+        and:
+        controlUnit.powerOn()
+        controlUnit.process Command.FILL.name()
+        controlUnit.process someVolume
+        controlUnit.process someVolume
+        controlUnit.process someVolume
+
+        when:
+        controlUnit.process request
+
+        then:
+        1 * storageMock.fill(Resource.DisposableCups, volume)
+
+        and:
+        1 * displayMock.accept(ControlState.MainMenu.prompt)
+
+        where:
+        someVolume = '9000'
+        request << ['0', '2000', '5000']
+
+        and:
+        volume = request.toInteger()
+    }
+
     def 'should process request "buy"'() {
         given:
-        loadResources water, milk, beans, cups, cash
+        controlUnit = new ControlBlock(displayMock, storageMock)
+        controlUnit.powerOn()
 
         when:
         controlUnit.process Command.BUY.name()
 
         then:
-        displayUnit.contains ControlState.BuyCoffee.prompt
-
-        where: 'the volume of resources in the storage unit'
-        water | milk | beans | cups | cash
-        0     | 0    | 0     | 0    | 0
-        400   | 540  | 20    | 9    | 550
+        1 * displayMock.accept(ControlState.BuyCoffee.prompt)
     }
-
-    def "Process"() {
-
-        given:
-        def controlUnit = new ControlBlock(displayUnit, storageUnit)
-
-        expect:
-        !controlUnit.isOperate()
-        displayUnit.text.isEmpty()
-
-        when:
-        controlUnit.process(_ as String)
-
-        then:
-        displayUnit.text.isEmpty()
-        0 * _
-    }
-
-
-    def 'should power on control block'() {
-
-        given:
-        def controlUnit = new ControlBlock(displayUnit, storageUnit)
-
-        expect:
-        !controlUnit.isOperate()
-        displayUnit.text.isEmpty()
-
-        when:
-        controlUnit.powerOn()
-
-        then:
-        controlUnit.isOperate()
-        displayUnit.contains ControlState.MainMenu.prompt
-    }
-
 }
