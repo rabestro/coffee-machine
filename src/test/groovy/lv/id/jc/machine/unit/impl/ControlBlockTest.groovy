3befc91b2ff9b7bb929270f736dfaf32adaab3ad
Jegors Čemisovs <jegors_cemisovs@epam.com>
2022-11-11 20:19:09 +0200

    add test 'should process request "take"'

--- src/test/groovy/lv/id/jc/machine/unit/impl/ControlBlockTest.groovy
+++ src/test/groovy/lv/id/jc/machine/unit/impl/ControlBlockTest.groovy
@@ -2,6 +2,7 @@ package lv.id.jc.machine.unit.impl
 
 import lv.id.jc.machine.model.Command
 import lv.id.jc.machine.model.ControlState
+import lv.id.jc.machine.model.Resource
 import lv.id.jc.machine.tag.UnitTest
 import lv.id.jc.machine.unit.ControlUnit
 import lv.id.jc.machine.unit.DisplayUnit
@@ -36,13 +37,57 @@ class ControlBlockTest extends Specification {
         !controlUnit.isOperate()
     }
 
-    def 'should process request "exit"'() {
+    def loadResources(water, milk, beans, cups, cash) {
+        with(storageUnit) {
+            volume(Resource.Water) >> water
+            volume(Resource.Milk) >> milk
+            volume(Resource.CoffeeBeans) >> beans
+            volume(Resource.DisposableCups) >> cups
+            volume(Resource.Cash) >> cash
+        }
+    }
+
+    def 'should process request "remaining"'() {
+
+        given:
+        loadResources water, milk, beans, cups, cash
+
         when:
-        controlUnit.process Command.EXIT.name()
+        controlUnit.process Command.REMAINING.name()
 
         then:
-        displayUnit.text.isBlank()
-        !controlUnit.isOperate()
+        displayUnit.contains report
+
+        where: 'the volume of resources in the storage unit'
+        water | milk | beans | cups | cash
+        0     | 0    | 0     | 0    | 0
+        400   | 540  | 20    | 9    | 550
+
+        report = """
+                The coffee machine has:
+                $water ml of water
+                $milk ml of milk
+                $beans g of coffee beans
+                $cups disposable cups
+                \$$cash of money
+                """.stripIndent()
+    }
+
+    def 'should process request "take"'() {
+
+        given:
+        loadResources water, milk, beans, cups, cash
+
+        when:
+        controlUnit.process Command.TAKE.name()
+
+        then:
+        displayUnit.contains report
+
+        where: 'the volume of resources in the storage unit'
+        water | milk | beans | cups | cash | report
+        0     | 0    | 0     | 0    | 0    | "I gave you \$0"
+        400   | 540  | 20    | 9    | 550  | "I gave you \$$cash"
     }
 
     def "Process"() {
