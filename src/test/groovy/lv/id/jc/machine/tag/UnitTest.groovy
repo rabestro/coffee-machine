073685598d93a13ff6b5fe14917d34ab70a76333
Jegors Čemisovs <jegors_cemisovs@epam.com>
2022-11-11 10:29:39 +0200

    add UnitTest annotation

---
+++ src/test/groovy/lv/id/jc/machine/tag/UnitTest.groovy
@@ -0,0 +1,13 @@
+package lv.id.jc.machine.tag
+
+import java.lang.annotation.ElementType
+import java.lang.annotation.Inherited
+import java.lang.annotation.Retention
+import java.lang.annotation.RetentionPolicy
+import java.lang.annotation.Target
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target([ElementType.TYPE, ElementType.METHOD])
+@Inherited
+@interface UnitTest {
+}
