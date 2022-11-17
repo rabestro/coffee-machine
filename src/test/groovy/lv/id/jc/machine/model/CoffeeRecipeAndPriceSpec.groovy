588df68bdd5375d013951c45b6075d1a5378c4c7
Jegors Čemisovs <jegors_cemisovs@epam.com>
2022-11-17 18:45:55 +0200

    update CoffeeRecipeAndPriceSpec

--- src/test/groovy/lv/id/jc/machine/model/CoffeeRecipeAndPriceSpec.groovy
+++ src/test/groovy/lv/id/jc/machine/model/CoffeeRecipeAndPriceSpec.groovy
@@ -18,16 +18,16 @@ class CoffeeRecipeAndPriceSpec extends Specification {
     def 'should have the correct recipe and price for #coffee'() {
 
         expect: 'coffee drink prepared according to the correct recipe'
-        coffee.recipe == recipe
+        coffee.recipe == expected_recipe
 
         and: 'a coffee drink is offered at an appropriate price'
-        coffee.price == price
+        coffee.price == expected_price
 
         where: 'the coffee drinks with correct recipe and fair price'
-        coffee            | price | recipe
-        Coffee.Espresso   | 4     | [(Water): 250, (CoffeeBeans): 16]
-        Coffee.Latte      | 7     | [(Water): 350, (Milk): 75, (CoffeeBeans): 20]
-        Coffee.Cappuccino | 6     | [(Water): 200, (Milk): 100, (CoffeeBeans): 12]
+        coffee            | expected_price | expected_recipe
+        Coffee.Espresso   | 4              | [(Water): 250, (CoffeeBeans): 16]
+        Coffee.Latte      | 7              | [(Water): 350, (Milk): 75, (CoffeeBeans): 20]
+        Coffee.Cappuccino | 6              | [(Water): 200, (Milk): 100, (CoffeeBeans): 12]
     }
 
 }
