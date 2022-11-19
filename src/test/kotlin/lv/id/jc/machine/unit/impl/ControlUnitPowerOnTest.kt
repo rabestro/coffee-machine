ab9b03c76e144506da91ec1b8fe7d5e72e59c5c8
Jegors Čemisovs <jegors_cemisovs@epam.com>
2022-11-19 23:30:35 +0200

    add ControlUnitPowerOnTest

---
+++ src/test/kotlin/lv/id/jc/machine/unit/impl/ControlUnitPowerOnTest.kt
@@ -0,0 +1,34 @@
+package lv.id.jc.machine.unit.impl
+
+import lv.id.jc.machine.model.ControlState
+import lv.id.jc.machine.unit.DisplayUnit
+import lv.id.jc.machine.unit.StorageUnit
+import org.junit.jupiter.api.*
+import org.junit.jupiter.api.Assertions.*
+import org.mockito.Mockito
+
+@Tag("Sanity")
+@DisplayName("The technician power on the control unit")
+internal class ControlUnitPowerOnTest {
+    private lateinit var storageUnit: StorageUnit
+    private lateinit var displayUnit: DisplayUnit
+
+    @BeforeEach
+    fun initMocks() {
+        storageUnit = Mockito.mock(StorageUnit::class.java)
+        displayUnit = Mockito.mock(DisplayUnit::class.java)
+    }
+
+    @Test
+    @DisplayName("should power on the control unit")
+    fun powerOn() {
+        val controlUnit = ControlBlock(displayUnit, storageUnit)
+        Assumptions.assumeFalse(controlUnit.isOperate())
+
+        controlUnit.powerOn()
+
+        assertTrue(controlUnit.isOperate(), "control unit does not turn on")
+        Mockito.verify(displayUnit).accept(ControlState.MainMenu.prompt)
+    }
+}
+
