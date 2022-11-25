331e99d6d2c849ee46ba3b2465a5c935183ed0cf
Jegors Čemisovs <jegors_cemisovs@epam.com>
2022-11-25 17:05:17 +0200

    add ControlBlockTest

---
+++ src/test/java/lv/id/jc/machine/unit/impl/ControlBlockTest.java
@@ -0,0 +1,32 @@
+package lv.id.jc.machine.unit.impl;
+
+import lv.id.jc.machine.model.ControlState;
+import lv.id.jc.machine.unit.DisplayUnit;
+import lv.id.jc.machine.unit.StorageUnit;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+//@Tag("Sanity")
+final class ControlBlockTest {
+    private StorageUnit storageUnit;
+    private DisplayUnit displayUnit;
+
+    @BeforeEach
+    void setUp() {
+        storageUnit = Mockito.mock(StorageUnit.class);
+        displayUnit = Mockito.mock(DisplayUnit.class);
+    }
+
+    @Test
+    void powerOn() {
+        var controlUnit = new ControlBlock(displayUnit, storageUnit);
+        controlUnit.powerOn();
+        assertTrue(controlUnit.isOperate(), controlUnit::toString);
+        Mockito.verify(displayUnit).accept(ControlState.MainMenu.getPrompt());
+    }
+}
+
